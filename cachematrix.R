1c3eb7159029e7c63755c9b2068ba9252cb2928c
shivani47 <shivaniarun47@gmail.com>
2016-11-13 01:13:44 +0530

    Add files via upload
    Programming assignment - Week 3

---
+++ cachematrix.R
@@ -0,0 +1,34 @@
+## Programming assignment 3 - 
+##make matrix vector that creates a list to set and get the value of the matrix and its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
+}
+
+## matrix that returns the inverse of the matrix 
+
+cacheSolve <- function(x, ...) {
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  matrixdata <- x$get()
+  m <- solve(matrixdata, ...)
+  x$setmatrix(m)
+  m
+}
+
+
+
+
